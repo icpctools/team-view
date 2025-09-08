3b6f25c64ccaa8f28a6e901ccd1fed0fd5037aad
Tim deBoer <git@tdeboer.ca>
2025-09-07 21:29:10 -0400

    Problem component
    Create a Problem component and share it between the scoreboard and problem page.
    Better reuse/consistency and opportunity to use it elsewhere in the future.

--- src/routes/problem/[id]/+page.svelte
+++ src/routes/problem/[id]/+page.svelte
@@ -1,32 +1,14 @@
 <script lang="ts">
-	import { parseHexColor, darker, rgbToHex } from '$lib/color-util.js';
+	import Problem from '$lib/ui/Problem.svelte';
 
 	let { data } = $props();
-
-	let pStyle = $state('');
-	let rgb = data.problem.rgb;
-	if (rgb) {
-		let col = parseHexColor(rgb);
-		let fg = '#fff';
-		if (col && col[0] + col[1] + col[2] > 450) {
-			fg = '#000';
-		}
-		let border =rgb;
-		if (col) {
-			border = rgbToHex(darker(col));
-		}
-
-		pStyle = 'background-color:'+rgb+';color:'+fg+ ';border-color:'+border+';';
-	} else {
-		pStyle = 'background-color:#fff;color:#000;border:#000;';
-	}
 </script>
 
 <div class="flex flex-col p-2 gap-1">
 	<div class="flex flex-col">
 	<div class="text-xl">Problem</div>
 	<div class="flex flex-row gap-x-2">
-		<div class="justify-self-center text-center uppercase border-[1px] rounded-sm w-12 h-6" style={pStyle}>{data.problem.label}</div>
+		<div class="w-12 h-6"><Problem problem={data.problem} /></div>
 		{data.problem.name}</div>
 </div>
 
@@ -39,8 +21,23 @@
 
 {#if data.submissions && data.submissions.length > 0}
 <div class="flex flex-col">
-	<div class="text-xl">Submissions</div>
-	<div>{data.submissions.length}</div>
+	<div class="text-xl">Submissions ({data.submissions.length})</div>
+
+	<div class="grid grid-table" style="grid-template-columns: 1fr 1fr 1fr 1fr" role="row">
+	<div role="cell" class="">Time</div>
+	<div role="cell" class="">Team</div>
+	<div role="cell" class="">Language</div>
+	<div role="cell" class="">Judgement</div>
+	</div>
+	{#each data.submissions as submission}
+	<div class="grid grid-table" style="grid-template-columns: 1fr 1fr 1fr 1fr" role="row">
+	<div role="cell" class="">{submission.time}</div>
+	<div role="cell" class="">{submission.team}</div>
+	<div role="cell" class="">{submission.language}</div>
+	<div role="cell" class="">{submission.judgement}</div>
+	</div>
+	{/each}
 </div>
 {/if}
+
 </div>
