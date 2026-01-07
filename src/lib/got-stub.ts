6aa98aab4899a0d733d182b8b78913f330c36260
Tim deBoer <git@tdeboer.ca>
2026-01-07 13:40:46 -0500

    Stub out got for build
    It's ugly, but stub out got package for client builds for now to avoid build errors.

---
+++ src/lib/got-stub.ts
@@ -0,0 +1,6 @@
+// Stub for got - this should never be used in client code
+// If you see this error, got is being imported in client-side code where it shouldn't be
+export default {};
+export const HTTPError = class extends Error {};
+export const RequestError = class extends Error {};
+
