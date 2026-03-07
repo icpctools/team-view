d6d5df4aa62d77109e0b16ed24b07b5a71e2dc0f
Tim deBoer <git@tdeboer.ca>
2026-03-07 10:06:54 -0500

    feat: start statuses
    Adds a start status component that shows the same statuses as the contest start
    presentation. When the contest has start statuses the component is added to the
    front page under the list of teams and automatically disappears once the
    contest is started. No animated state changes for now.
    
    Nothing major, just knocking off another basic component.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ src/lib/ui/StartStatusPanel.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { type StartStatus } from '@icpctools/contest-api';
+	import StartStatusUI from './StartStatusUI.svelte';
+
+	interface Props {
+		status?: StartStatus[];
+	}
+	let { status }: Props = $props();
+</script>
+
+{#if status}
+	<div class="grid grid-cols-3 px-4">
+		{#each status as stat (stat.id)}
+			<StartStatusUI status={stat} />
+		{/each}
+	</div>
+{/if}
