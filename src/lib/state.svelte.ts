ed700d4ae1c7c634a4cb39b44ff0a9aea3660e14
Tim deBoer <git@tdeboer.ca>
2026-03-30 13:24:35 -0400

    feat: use the event feed
    The Contest API has changed to support two modes: load individual endpoints, or
    use watch() to connect to the event feed. In the event feed mode it loads the
    access and scoreboard endpoints up front, connects to the feed, and waits for
    the first contest event (using a Mutex) before returning.
    
    Whenever there are changes to submissions or judgements the scoreboard is
    triggered as invalid, and a separate timer checks every 2s and reloads it.
    
    All of the calls to loadXxx() have been removed from the app to switch to the
    feed, and some minor changes to make all of the *server.ts consistent.
    
    There are several seconds of delay loading a large (e.g. finished) contest,
    some obvious optimizations to be made (e.g. #202 ignore runs) and undoubtedly
    a few bugs on initial loading/mutex, reconnection, reactivity, etc. However,
    it works, changes reach the UI much faster, we don't miss changes to other
    contest types / don't have to poll for more types. It's time to make this
    the default so that we can extend the app in new ways and start finding any
    remaining issues.
    
    Fixes #29.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -56,7 +56,7 @@ export async function loadContest(): Promise<ContestAPI | undefined> {
 		}
 
 		contest = contests.getContest(CONTEST?.contest_id);
-		contest?.watch();
+		await contest?.watch();
 		return contest;
 	} finally {
 		mutex.unlock();
