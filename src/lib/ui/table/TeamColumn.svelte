d2d3e86d28ad78ecd8cfc8812ecfa4b246614a13
Tim deBoer <git@tdeboer.ca>
2026-08-17 07:28:41 -0400

    fix: team icons always shown on scoreboard
    Incorrect ! sign causes the scoreboard to show icons beside each team
    even for contests that have no icons.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- src/lib/ui/table/TeamColumn.svelte
+++ src/lib/ui/table/TeamColumn.svelte
@@ -19,7 +19,7 @@
 			'cursor-pointer hover:bg-hover': onclick
 		}}>
 		<div class="flex flex-row items-center gap-x-2 max-w-full">
-			{#if logo || !noGap}
+			{#if logo || noGap}
 				<div class="w-6"><Logo ref={logo} /></div>
 			{/if}
 			<span class="overflow-hidden text-ellipsis">{team.display_name || team.name}</span>
